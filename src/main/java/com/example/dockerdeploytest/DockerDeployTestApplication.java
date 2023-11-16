093cd209a7caa91ac14d5072bac98eb09b20a70e
Joonyoung Hong <hjunyoung10@gmail.com>
2023-11-16 18:45:03 +0900

    Initial commit

---
+++ src/main/java/com/example/dockerdeploytest/DockerDeployTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerdeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDeployTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDeployTestApplication.class, args);
+    }
+
+}
